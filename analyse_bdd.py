cec049288e0c81577ece2d634b22672f1e137bf6
Sandra Lamiré <sandra.lamire.sl@gmail.com>
2023-11-10 13:58:13 +0100

    Suppression ouverture/fermeture cursor à chaque requête

--- analyse_bdd.py
+++ analyse_bdd.py
@@ -14,111 +14,93 @@ cursor.execute('SELECT COUNT(*) FROM dbo.Pingouins')
 # fetchone pour un seul résultat
 total_pingouins = cursor.fetchone()[0]
 print(f"Le nombre total de pingouins est : {total_pingouins}")
-cursor.close()
 
 print("#######################################################")
 
 # -	Pour chaque espèce, combien il y a-t-il d’individus ?
-cursor1 = connexion.cursor()
-cursor1.execute('SELECT espece, COUNT(*) AS nb_individus FROM dbo.Pingouins GROUP BY espece')
+cursor.execute('SELECT espece, COUNT(id_pingouin) FROM dbo.Pingouins GROUP BY espece')
 # fetchall pour récupérer tous les résultats sur plusieurs lignes
-resultats1 = cursor1.fetchall()
+resultats1 = cursor.fetchall()
 print("il y a :")
 for row in resultats1:
     espece, nb_individus_par_espece = row
     print(f"\t- {nb_individus_par_espece} individus de l'espèce {espece}")
-cursor1.close()
 
 print("#######################################################")
 
 # -	Combien il y a-t-il d’espèces ?
-cursor2 = connexion.cursor()
-cursor2.execute('SELECT espece, COUNT(*) FROM dbo.Pingouins GROUP BY espece')
-resultat_especes = cursor2.fetchall()
+cursor.execute('SELECT espece, COUNT(id_pingouin) FROM dbo.Pingouins GROUP BY espece')
+resultat_especes = cursor.fetchall()
 nb_especes = len(resultat_especes)
 print(f"Il y a {nb_especes} espèces différentes :")
 for espece in resultat_especes:
     print(f"\t- {espece[0]}")
-cursor2.close()
 
 print("#######################################################")
 
 # -	Pour chaque île, combien il y a-t-il d’individus ?
-cursor3 = connexion.cursor()
-cursor3.execute('SELECT ile, COUNT(*) AS nb_individus_par_ile FROM dbo.Pingouins GROUP BY ile')
+cursor.execute('SELECT ile, COUNT(id_pingouin) FROM dbo.Pingouins GROUP BY ile')
 # fetchall pour récupérer tous les résultats sur plusieurs lignes
-resultats2 = cursor3.fetchall()
+resultats2 = cursor.fetchall()
 print("Il y a : ")
 for row in resultats2:
     ile, nb_individus_par_ile = row
     print(f"\t- {nb_individus_par_ile} individus sur l'île de {ile}")
-cursor3.close()
 
 print("#######################################################")
 
 # -	Combien il y a-t-il d’îles ?
-cursor4 = connexion.cursor()
-cursor4.execute('SELECT ile, COUNT(*) FROM dbo.Pingouins GROUP BY ile')
-resultats_iles = cursor4.fetchall()
+cursor.execute('SELECT ile, COUNT(id_pingouin) FROM dbo.Pingouins GROUP BY ile')
+resultats_iles = cursor.fetchall()
 nb_iles = len(resultats_iles)
 print(f"Il y a {nb_iles} îles")
 for ile in resultats_iles:
     print(f"\t- {ile[0]}")
-cursor4.close()
 
 print("#######################################################")
 
 # -	Quelle est la longueur moyenne des becs ?
-cursor5 = connexion.cursor()
-cursor5.execute('SELECT AVG(DISTINCT longueur_bec) FROM dbo.Pingouins')
-longueur_bec_moyenne = round(cursor5.fetchone()[0], 2)
+cursor.execute('SELECT AVG(longueur_bec) FROM dbo.Pingouins')
+longueur_bec_moyenne = round(cursor.fetchone()[0], 2)
 print(f"La longueur moyenne des becs est de {longueur_bec_moyenne} mm")
-cursor5.close()
 
 print("#######################################################")
 
 # -	Quelle est la plus grande profondeur de bec ?
-cursor6 = connexion.cursor()
-cursor6.execute('SELECT MAX(DISTINCT profondeur_bec) FROM dbo.Pingouins')
-profondeur_bec_max = round(cursor6.fetchone()[0], 2)
+cursor.execute('SELECT MAX(profondeur_bec) FROM dbo.Pingouins')
+profondeur_bec_max = round(cursor.fetchone()[0], 2)
 print(f"La plus grande profondeur de bec est de {profondeur_bec_max} mm")
-cursor6.close()
 
 print("#######################################################")
 
 # -	Combien il y a-t-il de pingouins pour chaque sexe ?
-cursor7 = connexion.cursor()
-cursor7.execute('SELECT sex, COUNT(*) FROM dbo.Pingouins GROUP BY sex')
-resultats_sexe = cursor7.fetchall()
+cursor.execute('SELECT sex, COUNT(id_pingouin) FROM dbo.Pingouins GROUP BY sex')
+resultats_sexe = cursor.fetchall()
 print("Il y a :")
 for row in resultats_sexe:
     sex, nb_individus_par_sexe = row
     sex_label = "de sexe indéterminé" if sex is None else "femelles" if sex == "female" else "mâles" if sex == "male" else sex
     print(f"\t- {nb_individus_par_sexe} individus {sex_label}")
-cursor7.close()
-
 
 print("#######################################################")
 
 # -	Quel est l’âge du plus jeune pingouin ?
-cursor8 = connexion.cursor()
-cursor8.execute('SELECT YEAR(GETDATE()) - MAX(annee_naissance) as age_min FROM dbo.Pingouins WHERE annee_naissance IS NOT NULL')
-age_min = cursor8.fetchone()[0]
+cursor.execute('SELECT YEAR(GETDATE()) - MAX(annee_naissance) as age_min FROM dbo.Pingouins WHERE annee_naissance IS NOT NULL')
+age_min = cursor.fetchone()[0]
 print(f"Le plus jeune pingouin a {age_min} ans")
-cursor8.close()
 
 print("#######################################################")
 
 # -	Quel est l’âge du pingouin le plus âgé ?
-cursor9 = connexion.cursor()
-cursor9.execute('SELECT YEAR(GETDATE()) - MIN(annee_naissance) as age_max FROM dbo.Pingouins WHERE annee_naissance IS NOT NULL')
-age_max = cursor9.fetchone()[0]
+cursor.execute('SELECT YEAR(GETDATE()) - MIN(annee_naissance) as age_max FROM dbo.Pingouins WHERE annee_naissance IS NOT NULL')
+age_max = cursor.fetchone()[0]
 print(f"Le pingouin le plus âgée a {age_max} ans")
-cursor9.close()
 
 print("#######################################################")
+cursor.close()
 connexion.close()
 
+# Ecriture des résultats dans le fichier data/resultats_bdd.txt
 with open("data/resultats_bdd.txt", "w", encoding="UTF-8") as resultats_file:
     resultats_file.write(f"Nombre total de pingouins : {total_pingouins}\n")
     resultats_file.write("\n")
